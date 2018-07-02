19c9ec2aeb9a3ce041c3eccb6d40adcf2404afc9
leo <leo_1994@qq.com>
2018-07-02 14:01:25 +0800

    init project

---
+++ src/test/java/org/leo/calculation/CalculationApplicationTests.java
@@ -0,0 +1,16 @@
+package org.leo.calculation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
